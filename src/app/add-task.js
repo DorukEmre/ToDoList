3c5a9bce37c78cf7fb26b004123030521c84f3f8
Doruk Emre <devdoruk@outlook.com>
2022-03-08 14:02:09 +0000

    Separated form functions

---
+++ src/app/add-task.js
@@ -0,0 +1,26 @@
+import { addTaskToLibrary } from "./add-to-library";
+import { createCard } from "./create-card";
+import { fillCard } from "./fill-card";
+import { hideAddTaskForm } from "./form-management";
+import { myTasks } from "./tasks-management";
+
+export const userAddTask = () => {
+    const addTaskForm = document.querySelector("#add-task-form");
+    const projectTitle = addTaskForm.elements['form-projectTitle'].value;
+    const taskTitle = addTaskForm.elements['form-taskTitle'].value;
+    const dueDate = addTaskForm.elements['form-dueDate'].value;
+    const description = addTaskForm.elements['form-description'].value;
+    const formPriority = addTaskForm.elements['form-priority'].value;
+    const highPriority = (formPriority === "Yes") ? true : false;
+    
+    let refNumberOfNewTask = 0;
+    // (ref of new book) is (highest ref of myLibrary + 1)
+    if (myTasks.length > 0) {
+        refNumberOfNewTask = (myTasks.reduce((a, b) => (a.ref > b.ref) ? a : b).ref) + 1;
+    }
+
+    addTaskToLibrary(refNumberOfNewTask, projectTitle, taskTitle, description, dueDate, highPriority, false);
+    createCard(refNumberOfNewTask);
+    fillCard(refNumberOfNewTask);
+    hideAddTaskForm();
+}
